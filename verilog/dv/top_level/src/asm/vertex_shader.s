46aa62b2b14cb898c228b817ac1b111365c1d5c0
Michael Drobot <mdrobot7@iastate.edu>
2025-11-18 23:11:16 -0600

    top_level: Vertex shader bugfixes
    Load immediate and multiply immediate are giant pains in my ass
    
    Anything that involves fixed point math requires some serious mental
    gymnastics, but it works now.

--- verilog/dv/top_level/src/asm/vertex_shader.s
+++ verilog/dv/top_level/src/asm/vertex_shader.s
@@ -14,36 +14,39 @@
 ; unrolled for better performance. Bunching up memory
 ; accesses will result in better cache hit rates.
 
+() clrp (111)
+
 ; Thread ID (tid) is loaded into r0 by the core controller.
 ; Calculate the memory address of our triangle.
-() muli $tid, $tid, 4
+() sll $tid, $tid, 2
 () add $tid, $tid, $g46
 
 ; Load triangle indices
 () lw $r1, 0[$tid]
 () lw $r2, 4[$tid]
 () lw $r3, 8[$tid]
-() muli $r1, $r1, 20
-() muli $r2, $r2, 20
-() muli $r3, $r3, 20
+() li $r4, 20.000000
+() mul $r1, $r1, $r4
+() mul $r2, $r2, $r4
+() mul $r3, $r3, $r4
 
 ; Load vertex 1
 () lw $r4, 0[$r1] ; vx
 () lw $r5, 4[$r1] ; vy
 () lw $r6, 8[$r1] ; vz
 () andi $r7, $r7, 0 ; Homogeneous term
-() ori $r7, $r7, 1
+() ori $r7, $r7, 1.000000
 () lw $r12, 12[$r1] ; tx
 () lw $r13, 16[$r1] ; ty
 
 ; Vertex shade 1
-() dot4 $g0, $r5
+() dot4 $g0, $r4
 () macrd $r8
-() dot4 $g4, $r5
+() dot4 $g4, $r4
 () macrd $r9
-() dot4 $g8, $r5
+() dot4 $g8, $r4
 () macrd $r10
-() dot4 $g12, $r5
+() dot4 $g12, $r4
 () macrd $r11
 
 ; Ship to rasterizer
@@ -53,19 +56,17 @@
 () lw $r4, 0[$r2]
 () lw $r5, 4[$r2]
 () lw $r6, 8[$r2]
-() andi $r7, $r7, 0
-() ori $r7, $r7, 1
 () lw $r12, 12[$r2]
 () lw $r13, 16[$r2]
 
 ; Vertex shade 2
-() dot4 $g0, $r5
+() dot4 $g0, $r4
 () macrd $r8
-() dot4 $g4, $r5
+() dot4 $g4, $r4
 () macrd $r9
-() dot4 $g8, $r5
+() dot4 $g8, $r4
 () macrd $r10
-() dot4 $g12, $r5
+() dot4 $g12, $r4
 () macrd $r11
 
 ; Ship to rasterizer
@@ -75,19 +76,17 @@
 () lw $r4, 0[$r3]
 () lw $r5, 4[$r3]
 () lw $r6, 8[$r3]
-() andi $r7, $r7, 0
-() ori $r7, $r7, 1
 () lw $r12, 12[$r3]
 () lw $r13, 16[$r3]
 
 ; Vertex shade 3
-() dot4 $g0, $r5
+() dot4 $g0, $r4
 () macrd $r8
-() dot4 $g4, $r5
+() dot4 $g4, $r4
 () macrd $r9
-() dot4 $g8, $r5
+() dot4 $g8, $r4
 () macrd $r10
-() dot4 $g12, $r5
+() dot4 $g12, $r4
 () macrd $r11
 
 ; Ship to rasterizer
