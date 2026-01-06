aebf3be0b7e8ff5b1002ba9d4ebf96d2f4aef932
Colin McGann <colinm22@iastate.edu>
2026-01-05 22:04:36 -0600

    Added some extra config to the debug logger

--- verilog/rtl/test/debug_log.v
+++ verilog/rtl/test/debug_log.v
@@ -1,5 +1,3 @@
-// `define DL_ENABLE
-
 `define DL_NAME_MAX_LEN (256)
 
 `define DL_RED     "\033[31m"
@@ -9,10 +7,24 @@
 `define DL_MAGENTA "\033[35m"
 `define DL_CYAN    "\033[36m"
 
+`ifdef DL_RANDOM_COLORS
+`define DL_DEFINE(name, label, color, enabled) \
+    localparam name``_label   = label; \
+    wire [5 * 8 - 1:0] name``_colors [4:0]; \
+    assign name``_colors[0] = `DL_GREEN; \
+    assign name``_colors[1] = `DL_YELLOW; \
+    assign name``_colors[2] = `DL_BLUE; \
+    assign name``_colors[3] = `DL_MAGENTA; \
+    assign name``_colors[4] = `DL_CYAN; \
+    wire [5 * 8 - 1:0] name``_color; \
+    assign name``_color   = name``_colors[{$random} % 5]; \
+    localparam name``_enabled = enabled ? 1'b1 : 1'b0;
+`else
 `define DL_DEFINE(name, label, color, enabled) \
     localparam name``_label   = label; \
     localparam name``_color   = color; \
     localparam name``_enabled = enabled ? 1'b1 : 1'b0;
+`endif
 
 `ifdef DL_ENABLE
 `define DL(name, msg) \
