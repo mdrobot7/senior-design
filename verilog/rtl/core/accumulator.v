17c2b44406630e3a6dc0b3a22e5222934d2a7aa0
jarceo <jarceo@iastate.edu>
2026-01-25 16:49:34 -0600

    use add wrapper

--- verilog/rtl/core/accumulator.v
+++ verilog/rtl/core/accumulator.v
@@ -13,11 +13,14 @@ module accumulator_m (
     output reg  signed[`WORD_WIDTH-1:0] acc_o
 );
 
+    wire signed [`WORD_WIDTH-1:0] sum;
+    add_m #(.WIDTH(`WORD_WIDTH)) adder (.a_i(acc_o), .b_i(a_i), .y_o(sum));
+    
     always @(posedge clk_i, negedge nrst_i) begin
         if (!nrst_i) begin : RESET
             acc_o <= '0;
         end else if (en_i) begin : ACCUMULATE
-            acc_o <= acc_o + a_i;
+            acc_o <= sum;
         end
     end
 
