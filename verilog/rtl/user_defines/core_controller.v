579daffc27875137939e75388f2f8de1afee0bd6
Michael Drobot <mdrobot7@iastate.edu>
2026-02-08 02:42:39 -0600

    user_defines: Core count and core controller defs

---
+++ verilog/rtl/user_defines/core_controller.v
@@ -0,0 +1,8 @@
+/*
+ * Internal file, DO NOT INCLUDE! Only include user_defines.v!
+ */
+
+`define CORE_CTRL_CMD_STOP (0)
+`define CORE_CTRL_CMD_STOP (1)
+`define CORE_CTRL_CMD_STOP (2)
+`define CORE_CTRL_CMD_STOP (3)
