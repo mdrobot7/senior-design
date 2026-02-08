b7be7157a40aae853afb17f83e421186d22178d7
Michael Drobot <mdrobot7@iastate.edu>
2026-02-08 02:34:47 -0600

    core_controller: index_fetch: Cleanup, comment block, and additional IO pins
    Clean this code up so it doesn't look terrible and add the comment block
    above the module.
    
    Add a disable mechanism so it doesn't try to prefetch indices while you're
    still filling the index buffer/index buffer addr.
    
    Make the index buffer address programmable so you can fill a model into RAM
    while another is getting rendered and then just swap out the pointer. Also
    adds a layer of safety for various hardware issues

--- verilog/rtl/core_controller/index_fetch.v
+++ verilog/rtl/core_controller/index_fetch.v
@@ -1,5 +1,20 @@
+/*
+* Index buffer (pre)fetcher. When enabled, it will
+* immediately start fetching indices from the index
+* buffer and caching them in a FIFO. As the dispatcher
+* pulls indices out, the index fetcher will attempt
+* to refill them.
+*
+* The dispatcher dispatches at 1 index per clock cycle
+* and will always be ahead of the index fetcher. The
+* index fetcher will try to refill the FIFO, but due
+* to PKBus arbitration and memory chip setup time it
+* won't ever be able to catch up. TLDR we shouldn't
+* thrash PKBus, it should be one continuous stream read.
+*/
+
 module index_fetch_m #(
-  parameter INDEX_BUFFER_ADDR = 0
+  parameter CACHE_LEN_WORDS = 64
 ) (
   input wire clk_i,
   input wire nrst_i,
@@ -8,27 +23,26 @@ module index_fetch_m #(
   input wire [`BUS_MIPORT] mport_i,
   output reg [`BUS_MOPORT] mport_o,
 
-  input wire pause_i,               // 1: Pause index fetching
+  input wire         enable_i,            // 1: enable index fetching
+  input wire [`WORD] index_buffer_addr_i,
   input wire [`WORD] num_dispatches_i,
-  input wire model_done_clr_i,      // 1: Clear model done flag
-  output reg model_done_o,          // 1: Fetched all indices in this model
+  input wire         model_done_clr_i,    // 1: Clear model done flag
+  output reg         model_done_o,        // 1: Fetched all indices in this model
 
   // Index output stream
   input  wire [`STREAM_MIPORT(`WORD_WIDTH)] mstream_i,
   output wire [`STREAM_MOPORT(`WORD_WIDTH)] mstream_o
 );
 
-  localparam CACHE_LEN_WORDS = 64;
-
   localparam STATE_READY = 0;
-  localparam STATE_PREP = 1;
-  localparam STATE_READ = 2;
-  localparam STATE_DONE = 3;
+  localparam STATE_PREP  = 1;
+  localparam STATE_READ  = 2;
+  localparam STATE_DONE  = 3;
 
   reg  [`STREAM_SIPORT(`WORD_WIDTH)] sstreami;
   wire [`STREAM_SOPORT(`WORD_WIDTH)] sstreamo;
-  wire fifo_full  = !sstreamo[`STREAM_SO_READY];
-  wire fifo_empty = mstreamo[`STREAM_MO_VALID];
+  wire                               fifo_full  = !sstreamo[`STREAM_SO_READY];
+  wire                               fifo_empty = mstreamo[`STREAM_MO_VALID];
   stream_fifo_m #(
       `WORD_WIDTH,
       CACHE_LEN_WORDS
@@ -44,7 +58,7 @@ module index_fetch_m #(
   );
 
   reg [`WORD] index_buffer_offset;
-  reg [2:0] state;
+  reg [2:0]   state;
 
   always @(posedge clk_i, negedge nrst_i) begin
     if (nrst_i) begin
@@ -52,16 +66,16 @@ module index_fetch_m #(
       model_done_o <= 0;
 
       sstreami <= 0;
-      state <= STATE_READY;
       index_buffer_offset <= 0;
+      state <= STATE_READY;
     end
     else if (clk_i) begin
       case (state)
         STATE_READY: begin
-          if (!fifo_full && !pause_i && !model_done_o)
+          if (enable_i && !fifo_full && !model_done_o)
             state <= STATE_PREP;
         STATE_PREP: begin
-          mport_o[`BUS_MO_ADDR] <= INDEX_BUFFER_ADDR + index_buffer_offset;
+          mport_o[`BUS_MO_ADDR] <= index_buffer_addr_i + index_buffer_offset;
           mport_o[`BUS_MO_RW] <= `BUS_READ;
           mport_o[`BUS_MO_SIZE] <= `BUS_SIZE_STREAM;
           mport_o[`BUS_MO_REQ] <= 1;
@@ -70,7 +84,7 @@ module index_fetch_m #(
         end
         STATE_READ: begin
           if (mport_i[`BUS_MI_SEQSLV]) begin
-            sstreami[`STREAM_SI_DATA(`WORD_WIDTH)] <= mport_i[`BUS_MI_DATA]; 
+            sstreami[`STREAM_SI_DATA(`WORD_WIDTH)] <= mport_i[`BUS_MI_DATA];
             sstreami[`STREAM_SI_VALID(`WORD_WIDTH)] <= 1;
 
             index_buffer_offset <= index_buffer_offset + 1;
@@ -78,8 +92,8 @@ module index_fetch_m #(
           else
             sstreami[`STREAM_SI_VALID(`WORD_WIDTH)] <= 0;
 
-          if (pause_i || fifo_full || index_buffer_offset >= num_dispatches_i - 1) begin
-            // Stop on pause, fifo overrun, or index buffer overrun
+          if (fifo_full || index_buffer_offset >= num_dispatches_i - 1) begin
+            // Stop on fifo overrun, or index buffer overrun
             mport_o[`BUS_MO_SEQMST] <= 1;
           end
 
