7f5a6edb7416a883bfba7e9f2b9f05772d44c712
Zaba821 <114114833+Zaba821@users.noreply.github.com>
2026-02-12 16:28:06 -0600

    fixed compile errorrs

--- verilog/rtl/wbslave_pkmaster/wb_to_pk.v
+++ verilog/rtl/wbslave_pkmaster/wb_to_pk.v
@@ -5,53 +5,54 @@ module wb_to_pk #(
     // Wb slave
     input wire wb_clk_i,            // clock
     input wire wb_rst_i,            // Reset
-    input wire wbs_stb_i,           // Cycle strobe
-    input wire wbs_cyc_i,           // cycle in progress
-    input wire wbs_we_i,             // write en
-    input wire [3:0] wbs_sel_i,     // byte select
+    input wire wbs_stb_i,           // Cycle strobe (asserts when ready to trasnsfer data)
+    input wire wbs_cyc_i,           // cycle in progress (transfer)
+    output reg wbs_ack_o,           // acknowledge (asserted indicates termination of cycle)
+
     input wire [31:0] wbs_dat_i,    // data in 
     input wire [31:0] wbs_adr_i,    // address
-    output reg wbs_ack_o,           // acknowledge
     output reg [31:0] wbs_dat_o,    // data out
 
-    // PKbus master
-    input wire [`BUS_MIPORT] mport_i,
-    output wire [`BUS_MOPORT] mport_o,
+    input wire wbs_we_i,             // write en 
 
-    output reg pk_req_o,
-    input wire pk_ack_i,
+
+    // PKbus master
+    input wire [`BUS_MIPORT] mport_i, 
+    output wire [`BUS_MOPORT] mport_o, 
+
+    output reg pk_req_o, 
+    input wire pk_ack_i, 
     output reg pk_rw_o, // WE
 
     output reg [1:0] pk_size_o,
 
     output reg [`BUS_ADDR_PORT] pk_addr_o,
 
-    input wire [`BUS_DATA_PORT] pk_data_i
-    output reg [`BUS_DATA_PORT] pk_data_o,
+    input wire [`BUS_DATA_PORT] pk_data_i,
+    output reg [`BUS_DATA_PORT] pk_data_o
 );
 
-    localparam STATE_WAIT_STB  = 2'b00; 
-    localparam STATE_WAIT_PK  = 2'b01;
-    localparam STATE_SET_ACK  = 2'b10;
-    localparam STATE_WAIT_ACK = 2'b11;
+    localparam STATE_WAIT  = 2'b00; 
+    localparam STATE_TRANSFER = 2'b01;
+    localparam STATE_ACK  = 2'b10;
 
     reg [1:0] cur_state, next_state;
 
-    always@ (posedge wb_clk_i, posedge wb_rst_i) begin
-        if (wb_rst_i)
-            cur_state <= STATE_WAIT_STB;
-        else
-            cur_state <= next_state;
-    end
+    always@ (posedge wb_clk_i or posedge wb_rst_i) begin
+        if (wb_rst_i) begin
+        //what does reset do
+            cur_state <= STATE_WAIT;
 
-    always@ (*) begin
-        next_state = STATE_WAIT_STB; // default
+        end
+	else begin
+
+        next_state = STATE_WAIT; // default
         pk_size_o = 2'b10; // default size, word
 
         case (cur_state) 
 
-            STATE_WAIT_STB: begin
-                wbs_ack_o   = 1'b0; // init req and ack 0
+            STATE_WAIT: begin
+                wbs_ack_o   = 1'b0; 
                 pk_req_o    = 1'b0;
 
                 if (wbs_stb_i && wbs_cyc_i) begin
@@ -59,28 +60,25 @@ module wb_to_pk #(
                     pk_data_o   = wbs_dat_i;
                     pk_rw_o     = wbs_we_i;
                     pk_req_o    = 1'b1;
-                    next_state  = STATE_WAIT_PK;
+                    next_state  = STATE_TRANSFER;
                 end
             end
 
-            STATE_WAIT_PK: begin
-                if (pk_ack_i)
-                    next_state  = STATE_SET_ACK;
-            end
-
-            STATE_SET_ACK: begin
+            STATE_TRANSFER: begin
                 wbs_ack_o   = 1'b1;
                 wbs_dat_o   = wbs_we_i ? 0 : pk_data_i;
-            end      
+                next_state = STATE_ACK;
+            end  
 
-            STATE_WAIT_ACK: begin
+            STATE_ACK: begin
                 if (!pk_ack_i) begin
+                    pk_req_o = 1'b0;
                     wbs_ack_o   = 1'b0;
-                    pk_req_o    = 1'b0;
-                    next_state  = STATE_WAIT_STB;
+                    next_state  = STATE_WAIT;
                 end
             end
 
         endcase
     end
+end
 endmodule
