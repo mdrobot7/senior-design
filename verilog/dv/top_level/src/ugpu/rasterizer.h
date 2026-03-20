ba50cfe80d32f6418d0b3f93d34a2ce8b5d4311d
Michael Drobot <mdrobot7@iastate.edu>
2026-03-20 03:38:43 -0500

    top_level: src: ugpu: uGPU device headers (CMSIS-style)
    Create the full device headers for the uGPU. Follows the CMSIS header
    style, using sub-structs for individual registers and plenty of
    macros. Should be easy to pick up for anyone coming from ARM.
    
    Still need to work out the makefile stuff for this tho

---
+++ verilog/dv/top_level/src/ugpu/rasterizer.h
@@ -0,0 +1,36 @@
+#ifndef _UGPU_RASTERIZER_H
+#define _UGPU_RASTERIZER_H
+
+#include <cstdint>
+#include <stdint.h>
+
+#define RAST_TEXADDR_ADDR_Pos    (0)
+#define RAST_TEXADDR_ADDR_Msk    (0xFFFFFFFFUL << RAST_TEXADDR_ADDR_Pos)
+#define RAST_TEXADDR_ADDR(value) (RAST_TEXADDR_ADDR_Msk & ((value) << RAST_TEXADDR_ADDR_Pos))
+typedef union {
+  struct {
+    uint32_t ADDR : 32;
+  } bit;
+  uint32_t reg;
+} RAST_TEXADDR_t;
+
+#define RAST_TEXDIM_WIDTH_Pos     (0)
+#define RAST_TEXDIM_WIDTH_Msk     (0xFFFFUL << RAST_TEXDIM_WIDTH_Pos)
+#define RAST_TEXDIM_WIDTH(value)  (RAST_TEXDIM_WIDTH_Msk & ((value) << RAST_TEXDIM_WIDTH_Pos))
+#define RAST_TEXDIM_HEIGHT_Pos    (16)
+#define RAST_TEXDIM_HEIGHT_Msk    (0xFFFFUL << RAST_TEXDIM_HEIGHT_Pos)
+#define RAST_TEXDIM_HEIGHT(value) (RAST_TEXDIM_HEIGHT_Msk & ((value) << RAST_TEXDIM_HEIGHT_Pos))
+typedef union {
+  struct {
+    uint32_t WIDTH  : 16;
+    uint32_t HEIGHT : 16;
+  } bit;
+  uint32_t reg;
+} RAST_TEXDIM_t;
+
+typedef struct {
+  RAST_TEXADDR_t TEXADDR;
+  RAST_TEXDIM_t TEXDIM;
+} RAST_t;
+
+#endif
