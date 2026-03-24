aa77a81498f2d78956cfd5392615cb83479e413a
Michael Drobot <mdrobot7@iastate.edu>
2026-03-24 15:27:45 -0500

    top_level: src: Colin comment fixes

--- verilog/dv/top_level/src/demos/demos.c
+++ verilog/dv/top_level/src/demos/demos.c
@@ -7,7 +7,7 @@ uint32_t fragment_shader_len;
 
 void demos_init() {
   vertex_shader       = &_svertex_shader;
-  vertex_shader_len   = &_evertex_shader - &_svertex_shader;
+  vertex_shader_len   = (uint32_t) (&_evertex_shader - &_svertex_shader);
   fragment_shader     = &_sfragment_shader;
-  fragment_shader_len = &_efragment_shader - &_sfragment_shader;
+  fragment_shader_len = (uint32_t) (&_efragment_shader - &_sfragment_shader);
 }
--- verilog/dv/top_level/src/ugpu/math.h
+++ verilog/dv/top_level/src/ugpu/math.h
@@ -9,6 +9,6 @@
 #define UGPU_FLOAT_TO_FIXED(n) ((int32_t) ((float) (n) * (1 << UGPU_DECIMAL_POS)))
 #define UGPU_FIXED_TO_FLOAT(n) ((float) (n) / (1 << UGPU_DECIMAL_POS))
 
-#define UGPU_FIXED_MUL(a, b) ((((uint64_t) a) * (uint64_t(b))) >> (UGPU_DECIMAL_POS * 2))
+#define UGPU_FIXED_MUL(a, b) ((((int64_t) a) * ((int64_t) b)) >> (UGPU_DECIMAL_POS * 2))
 
 #endif
