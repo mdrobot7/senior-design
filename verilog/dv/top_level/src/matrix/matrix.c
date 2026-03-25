6795198ff0436d8245f238e748374b27649350e1
Michael Drobot <mdrobot7@iastate.edu>
2026-03-25 03:08:43 -0500

    top_level: matrix: Matrix library for MVP calculations
    Quick little matrix library, should handle all of the basic ops
    that we need. Rotation is hella slow, but I don't really care since
    harder scenes will be run with an external host anyway.
    
    **NEEDS PICOLIBC!** sin() and cos() are part of math.h and aren't
    in the build system by default. I need to go and add picolibc soon

---
+++ verilog/dv/top_level/src/matrix/matrix.c
@@ -0,0 +1,79 @@
+#include "matrix.h"
+
+#include <math.h>
+#include <string.h>
+
+#define PI_180_FIXED UGPU_FLOAT_TO_FIXED(3.14159265f / 180.0f)
+
+void mat4_identity(mat4_t m) {
+  memset(m, 0, 4 * 4 * sizeof(ugpu_fp_t));
+  m[0][0] = UGPU_FIXED(1);
+  m[1][1] = UGPU_FIXED(1);
+  m[2][2] = UGPU_FIXED(1);
+  m[3][3] = UGPU_FIXED(1);
+}
+
+void mat4_trans(mat4_t m, ugpu_fp_t x, ugpu_fp_t y, ugpu_fp_t z) {
+  memset(m, 0, 4 * 4 * sizeof(ugpu_fp_t));
+  m[0][3] = x;
+  m[1][3] = y;
+  m[2][3] = z;
+  m[3][3] = UGPU_FIXED(1);
+}
+
+void mat4_scale(mat4_t m, ugpu_fp_t sx, ugpu_fp_t sy, ugpu_fp_t sz) {
+  memset(m, 0, 4 * 4 * sizeof(ugpu_fp_t));
+  m[0][0] = sx;
+  m[1][1] = sy;
+  m[2][2] = sz;
+  m[3][3] = UGPU_FIXED(1);
+}
+
+void mat4_rotate(mat4_t m, ugpu_fp_t rad_x, ugpu_fp_t rad_y, ugpu_fp_t rad_z) {
+  // Taken from here https://sites.google.com/site/glennmurray/glenn-murray-ph-d/rotation-matrices-and-formulas/rotation-about-an-arbitrary-axis-in-3-dimensions#h.ody1q8jzvr8u
+  // TODO: Rotation around an arbitrary axis (quaternions)?
+  memset(m, 0, 4 * 4 * sizeof(ugpu_fp_t));
+
+  float sin_x = sin(UGPU_FIXED_TO_FLOAT(rad_x)); // TODO: Add picolibc to get math.h
+  float cos_x = cos(UGPU_FIXED_TO_FLOAT(rad_x));
+  float sin_y = sin(UGPU_FIXED_TO_FLOAT(rad_y));
+  float cos_y = cos(UGPU_FIXED_TO_FLOAT(rad_y));
+  float sin_z = sin(UGPU_FIXED_TO_FLOAT(rad_z));
+  float cos_z = cos(UGPU_FIXED_TO_FLOAT(rad_z));
+
+  m[0][0] = UGPU_FLOAT_TO_FIXED(cos_y * cos_z);
+  m[0][1] = UGPU_FLOAT_TO_FIXED(sin_x * sin_y * cos_z - cos_x * sin_z);
+  m[0][2] = UGPU_FLOAT_TO_FIXED(cos_x * sin_y * cos_z + sin_x * sin_z);
+
+  m[1][0] = UGPU_FLOAT_TO_FIXED(cos_y * sin_z);
+  m[1][1] = UGPU_FLOAT_TO_FIXED(sin_z * sin_y * sin_z + cos_x * cos_z);
+  m[1][2] = UGPU_FLOAT_TO_FIXED(cos_x * sin_y * sin_z - sin_x * cos_z);
+
+  m[2][0] = UGPU_FLOAT_TO_FIXED(-sin_y);
+  m[2][1] = UGPU_FLOAT_TO_FIXED(sin_x * cos_y);
+  m[2][2] = UGPU_FLOAT_TO_FIXED(cos_x * cos_y);
+
+  m[3][3] = UGPU_FIXED(1);
+}
+
+void mat4_rotated(mat4_t m, ugpu_fp_t deg_x, ugpu_fp_t deg_y, ugpu_fp_t deg_z) {
+  // Should be enough precision.... hopefully
+  mat4_rotate(m, UGPU_FIXED_MUL(deg_x, PI_180_FIXED), UGPU_FIXED_MUL(deg_y, PI_180_FIXED), UGPU_FIXED_MUL(deg_z, PI_180_FIXED));
+}
+
+void mat4_mul(mat4_t a, mat4_t b) {
+  mat4_t dest;
+  mat4_muld(dest, a, b);
+  memcpy(a, dest, 4 * 4 * sizeof(ugpu_fp_t));
+}
+
+void mat4_muld(mat4_t dest, mat4_t a, mat4_t b) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      dest[i][j] = 0;
+      for (int k = 0; k < 4; k++) {
+        dest[i][j] += UGPU_FIXED_MUL(a[i][k], b[k][j]);
+      }
+    }
+  }
+}
---
+++ verilog/dv/top_level/src/matrix/matrix.h
@@ -0,0 +1,22 @@
+#ifndef _MATRIX_H
+#define _MATRIX_H
+
+#include "ugpu.h"
+
+#include <stdint.h>
+
+// 4x4 fixed point matrix. Indices are mat4_t[ROW][COL]
+typedef ugpu_fp_t mat4_t[4][4];
+
+// Matrix constructors: overwrites the matrix m with a identity, translation, rotation... matrix
+void mat4_identity(mat4_t m);
+void mat4_trans(mat4_t m, ugpu_fp_t x, ugpu_fp_t y, ugpu_fp_t z);
+void mat4_scale(mat4_t m, ugpu_fp_t sx, ugpu_fp_t sy, ugpu_fp_t sz);
+void mat4_rotate(mat4_t m, ugpu_fp_t rad_x, ugpu_fp_t rad_y, ugpu_fp_t rad_z); // Applies x, then y, then z
+void mat4_rotated(mat4_t m, ugpu_fp_t deg_x, ugpu_fp_t deg_y, ugpu_fp_t deg_z);
+
+// Matrix multiplication
+void mat4_mul(mat4_t a, mat4_t b);
+void mat4_muld(mat4_t dest, mat4_t a, mat4_t b);
+
+#endif
