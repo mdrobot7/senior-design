8a7a3b54c80cfd267fb5aa560cf7eb7fd8ca7c83
Michael Drobot <mdrobot7@iastate.edu>
2026-03-24 22:43:03 -0500

    core: regfile: Add stack pointer, split core and global regfile into separate modules
    Adding stack pointer would make this a 5 param module.... that's getting
    a little ridiculous. Just split them into dedicated modules at this point

--- verilog/rtl/core/regfile.v
+++ verilog/rtl/core/regfile.v
@@ -1,45 +1,38 @@
-/**
-*   Generic Register File
+/*
+* Core register file.
 *
-*   Can have variable widths/heights. Standard widths is 32 bits, and standard heights are 16 and 48
-*   Has a variable base address for regfile concatenation.
+* 16 word-width entries, indexed r0-r15. r15 is a
+* special register with a different reset value,
+* for use as a stack pointer. r15 can be reset
+* both synchronously and asynchronously.
 */
-module regfile_m #(
-    //How many bits are in each regfile entry
-    parameter REGFILE_WIDTH         = `WORD_WIDTH,
-    //How many entries there are
-    parameter REGFILE_HEIGHT        = 16,
-    //The first address of the register file
-    parameter REGFILE_BASE_ADDR     = 0,
-    //Truthy value for creation of a zero register at the maximum address
-    parameter HAS_ZERO_REG          = 0,
-    //Address space size
-    parameter REGFILE_ADDR_SIZE     = $clog2(REGFILE_HEIGHT + REGFILE_BASE_ADDR)
+module core_regfile_m #(
+    parameter SP = 32'h00000000
 ) (
     input wire clk_i,
     input wire nrst_i,
+    input wire nsync_rst_i,
 
     input wire wr_en_i,
-    input wire [REGFILE_ADDR_SIZE - 1:0] wr_addr_i,
-    input wire [REGFILE_WIDTH-1:0] wr_data_i,
+    input wire [`REG_DEST_WIDTH-1:0] wr_addr_i,
+    input wire [`WORD] wr_data_i,
 
-    input wire [REGFILE_ADDR_SIZE-1:0] r1_addr_i,
-    input wire [REGFILE_ADDR_SIZE-1:0] r2_addr_i,
+    input wire [`REG_DEST_WIDTH-1:0] r1_addr_i,
+    input wire [`REG_DEST_WIDTH-1:0] r2_addr_i,
 
-    output reg [REGFILE_WIDTH-1:0] r1_data_o,
-    output reg [REGFILE_WIDTH-1:0] r2_data_o,
+    output reg [`WORD] r1_data_o,
+    output reg [`WORD] r2_data_o,
 
-    input wire inbox_write_i,
-    input  wire[REGFILE_WIDTH * `CORE_MAILBOX_HEIGHT-1:0] inbox_i,
-    output reg[REGFILE_WIDTH * `CORE_MAILBOX_HEIGHT-1:0] outbox_o
+    input  wire inbox_write_i,
+    input  wire [`WORD_WIDTH * `CORE_MAILBOX_HEIGHT-1:0] inbox_i,
+    output reg  [`WORD_WIDTH * `CORE_MAILBOX_HEIGHT-1:0] outbox_o
 );
-    localparam REGFILE_HIGHEST_ADDR = REGFILE_HEIGHT + REGFILE_BASE_ADDR - 1;
-    reg [REGFILE_WIDTH - 1:0] mem [REGFILE_HIGHEST_ADDR:REGFILE_BASE_ADDR];
+    reg [`WORD] mem [`NUM_LOCAL_REGS-1:0];
 
     always @(posedge clk_i, negedge nrst_i) begin
         if (!nrst_i) begin : RESET
             integer i;
-            for (i = REGFILE_BASE_ADDR; i < REGFILE_HEIGHT + REGFILE_BASE_ADDR; i = i + 1) begin
+            for (i = 0; i < `NUM_LOCAL_REGS; i = i + 1) begin
                 mem[i] <= 0;
             end
         end
@@ -47,44 +40,31 @@ module regfile_m #(
             integer i;
             if(inbox_write_i) begin
                 for(i = 0; i < `CORE_MAILBOX_HEIGHT; i = i + 1) begin
-                    mem[i + 1] <= inbox_i[(i*REGFILE_WIDTH) +: REGFILE_WIDTH];
+                    mem[i + 1] <= inbox_i[(i*`WORD_WIDTH) +: `WORD_WIDTH];
                 end
             end
-            else if(wr_en_i) begin
-                if(!HAS_ZERO_REG) begin
-                    mem[wr_addr_i] <= wr_data_i;
-                end
-                else if(HAS_ZERO_REG) begin
-                    mem[wr_addr_i] <= (wr_addr_i == REGFILE_HIGHEST_ADDR ? 0 : wr_data_i);
-                end
-            end
+            else if(wr_en_i)
+                mem[wr_addr_i] <= wr_data_i;
+
+            if (!nsync_rst_i)
+              mem[`NUM_LOCAL_REGS-1] <= SP;
         end
     end
 
     always @(*) begin : READ
         integer i;
-        reg [REGFILE_WIDTH-1:0] r1_data;
-        reg [REGFILE_WIDTH-1:0] r2_data;
-        //Forwards data if current write = current read address
-        if(!HAS_ZERO_REG) begin
-            r1_data = ((r1_addr_i == wr_addr_i) && wr_en_i) ? wr_data_i : mem[r1_addr_i];
-            r2_data = ((r2_addr_i == wr_addr_i) && wr_en_i) ? wr_data_i : mem[r2_addr_i];
-        end else begin
-            r1_data = ((r1_addr_i == wr_addr_i) && wr_en_i && (wr_addr_i != REGFILE_HIGHEST_ADDR)) ? wr_data_i : mem[r1_addr_i];
-            r2_data = ((r2_addr_i == wr_addr_i) && wr_en_i && (wr_addr_i != REGFILE_HIGHEST_ADDR)) ? wr_data_i : mem[r2_addr_i];
-        end
+        reg [`WORD] r1_data;
+        reg [`WORD] r2_data;
 
-        if (r1_addr_i >= REGFILE_BASE_ADDR && r1_addr_i <= REGFILE_HIGHEST_ADDR)
-          r1_data_o = r1_data;
-        else
-          r1_data_o = 0;
-        if (r2_addr_i >= REGFILE_BASE_ADDR && r2_addr_i <= REGFILE_HIGHEST_ADDR)
-          r2_data_o = r2_data;
-        else
-          r2_data_o = 0;
+        //Forwards data if current write = current read address
+        r1_data = ((r1_addr_i == wr_addr_i) && wr_en_i) ? wr_data_i : mem[r1_addr_i];
+        r2_data = ((r2_addr_i == wr_addr_i) && wr_en_i) ? wr_data_i : mem[r2_addr_i];
+
+        r1_data_o = r1_data;
+        r2_data_o = r2_data;
 
         for(i = 0; i < `CORE_MAILBOX_HEIGHT; i = i + 1) begin
-            outbox_o[i*REGFILE_WIDTH +: REGFILE_WIDTH] = {mem[i + 1]};
+            outbox_o[i*`WORD_WIDTH +: `WORD_WIDTH] = {mem[i + 1]};
         end
     end
 
