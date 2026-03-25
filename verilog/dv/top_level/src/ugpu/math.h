3f3f9501f6399f48bbe63114304210890f47a337
Michael Drobot <mdrobot7@iastate.edu>
2026-03-25 03:08:17 -0500

    top_level: ugpu: Typedef for uGPU fixed point

--- verilog/dv/top_level/src/ugpu/math.h
+++ verilog/dv/top_level/src/ugpu/math.h
@@ -3,12 +3,15 @@
 
 #include <stdint.h>
 
+// Fixed point type
+typedef int32_t ugpu_fp_t;
+
 #define UGPU_DECIMAL_POS (16)
 
-#define UGPU_FIXED(n)          ((int32_t) (n << UGPU_DECIMAL_POS))
-#define UGPU_FLOAT_TO_FIXED(n) ((int32_t) ((float) (n) * (1 << UGPU_DECIMAL_POS)))
+#define UGPU_FIXED(n)          ((ugpu_fp_t) (n << UGPU_DECIMAL_POS))
+#define UGPU_FLOAT_TO_FIXED(n) ((ugpu_fp_t) ((float) (n) * (1 << UGPU_DECIMAL_POS)))
 #define UGPU_FIXED_TO_FLOAT(n) ((float) (n) / (1 << UGPU_DECIMAL_POS))
 
-#define UGPU_FIXED_MUL(a, b) ((((int64_t) a) * ((int64_t) b)) >> (UGPU_DECIMAL_POS * 2))
+#define UGPU_FIXED_MUL(a, b) ((ugpu_fp_t) ((((int64_t) a) * ((int64_t) b)) >> (UGPU_DECIMAL_POS * 2)))
 
 #endif
