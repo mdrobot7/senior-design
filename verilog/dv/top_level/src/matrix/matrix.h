36ed51fd1e8347506daadea21e07c08f8405a160
Michael Drobot <mdrobot7@iastate.edu>
2026-03-26 02:27:51 -0500

    top_level: src: matrix: MVP matrix constructors
    Constructors for view, perspective, and screen matrices.

--- verilog/dv/top_level/src/matrix/matrix.h
+++ verilog/dv/top_level/src/matrix/matrix.h
@@ -5,6 +5,8 @@
 
 #include <stdint.h>
 
+#define MAT4_SIZE (4 * 4 * sizeof(ugpu_fp_t))
+
 // 4x4 fixed point matrix. Indices are mat4_t[ROW][COL]
 typedef ugpu_fp_t mat4_t[4][4];
 
@@ -15,8 +17,16 @@ void mat4_scale(mat4_t m, ugpu_fp_t sx, ugpu_fp_t sy, ugpu_fp_t sz);
 void mat4_rotate(mat4_t m, ugpu_fp_t rad_x, ugpu_fp_t rad_y, ugpu_fp_t rad_z); // Applies x, then y, then z
 void mat4_rotated(mat4_t m, ugpu_fp_t deg_x, ugpu_fp_t deg_y, ugpu_fp_t deg_z);
 
+// MVP matrix constructors
+void mat4_view(mat4_t m, ugpu_fp_t cam_x, ugpu_fp_t cam_y, ugpu_fp_t cam_z, ugpu_fp_t cam_rad_x, ugpu_fp_t cam_rad_y, ugpu_fp_t cam_rad_z); // Creates a view matrix using a camera position and rotation
+void mat4_perspective(mat4_t m, ugpu_fp_t fov_radians, ugpu_fp_t z_near, ugpu_fp_t z_far);
+void mat4_screen(mat4_t m, uint32_t screen_width, uint32_t screen_height);
+
 // Matrix multiplication
 void mat4_mul(mat4_t a, mat4_t b);
 void mat4_muld(mat4_t dest, mat4_t a, mat4_t b);
+
+// Misc matrix operations
+void mat4_inv(mat4_t dest, mat4_t m);
 
 #endif
--- verilog/dv/top_level/src/ugpu/math.h
+++ verilog/dv/top_level/src/ugpu/math.h
@@ -12,6 +12,7 @@ typedef int32_t ugpu_fp_t;
 #define UGPU_FLOAT_TO_FIXED(n) ((ugpu_fp_t) ((float) (n) * (1 << UGPU_DECIMAL_POS)))
 #define UGPU_FIXED_TO_FLOAT(n) ((float) (n) / (1 << UGPU_DECIMAL_POS))
 
-#define UGPU_FIXED_MUL(a, b) ((ugpu_fp_t) ((((int64_t) a) * ((int64_t) b)) >> (UGPU_DECIMAL_POS * 2)))
+#define UGPU_FIXED_MUL(a, b) ((ugpu_fp_t) (((int64_t) (a) * (int64_t) (b)) >> UGPU_DECIMAL_POS))
+#define UGPU_FIXED_DIV(a, b) ((ugpu_fp_t) ((((int64_t) (a) << UGPU_DECIMAL_POS) / (int64_t) (b))))
 
 #endif
