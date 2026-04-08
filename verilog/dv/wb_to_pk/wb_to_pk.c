8411dbf1f0abf9f4219cd8fce416fce958ef65bd
Zaba821 <114114833+Zaba821@users.noreply.github.com>
2026-04-08 00:54:54 -0500

    more timing fixes

--- verilog/dv/wb_to_pk/wb_to_pk.c
+++ verilog/dv/wb_to_pk/wb_to_pk.c
@@ -58,14 +58,41 @@ void main() {
 
   wait_bridge();
 
+  if (ADDR != 0x0000001C) test_fail();
+  if (WCOUNT != 0x00000002) test_fail();
+  if (WDATA != 0x45520080) test_fail();
+
+  // pk stream write 224 words
+  ADDR   = 0x00000080;
+  WCOUNT = 0x000000E0;
+  WDATA  = 0x10804070;
+
+  wait_bridge();
+
   if (ADDR != 0x00000080) test_fail();
   if (WCOUNT != 0x000000E0) test_fail();
   if (WDATA != 0x10804070) test_fail();
 
   // wishbone read (pk read)
+
   volatile uint32_t readValue;
 
-  uint32_t current_addr = 0x0000000C;
+  uint32_t current_addr = 0x00000008;
+
+  for (int i = 0; i < 1; i++) {
+    ADDR = current_addr;
+    wait_bridge();
+    readValue = RDATA;
+
+    if (ADDR != current_addr) test_fail();
+    if (RDATA != 0xF18F20FF) test_fail();
+    if (readValue != 0xF18F20FF) test_fail();
+
+    current_addr += 4;
+  }
+
+
+  current_addr = 0x0000000C;
 
   for (int i = 0; i < 4; i++) {
     ADDR = current_addr;
@@ -94,7 +121,7 @@ void main() {
     current_addr += 4;
   }
 
-  // wishbone read (pk read) of 255 addresses
+  // wishbone read (pk read) of 224 addresses
   current_addr = 0x00000080;
 
   for (int i = 0; i < 224; i++) {
@@ -105,6 +132,39 @@ void main() {
     if (ADDR != current_addr) test_fail();
     if (RDATA != 0x10804070) test_fail();
     if (readValue != 0x10804070) test_fail();
+
+    current_addr += 4;
+  }
+
+  //clear all memory
+  // pk stream write 256 words
+  ADDR   = 0x00000000;
+  WCOUNT = 0x00000100;
+  WDATA  = 0x00000000;
+
+  wait_bridge();
+
+  if (ADDR != 0x00000000) test_fail();
+  if (WCOUNT != 0x00000100) test_fail();
+  if (WDATA != 0x00000000) test_fail();
+
+
+
+  //check to see if all values are set to 0
+  current_addr = 0x00000000;
+
+
+  for (int i = 0; i < 256; i++) {
+    ADDR = current_addr;
+    wait_bridge();
+    readValue = RDATA;
+
+    if (ADDR != current_addr) test_fail();
+    if (RDATA != 0x00000000) test_fail();
+    if (readValue != 0x00000000) test_fail();
+
+    current_addr += 4;
+  }
 
     current_addr += 4;
   }
--- verilog/rtl/wbslave_pkmaster/wb_to_pk.v
+++ verilog/rtl/wbslave_pkmaster/wb_to_pk.v
@@ -1,19 +1,19 @@
 module wb_to_pk_m
 (
-    input wire wb_clk_i,            
-    input wire wb_rst_i,  
+    input wire wb_clk_i,
+    input wire wb_rst_i,
 
-    input wire wbs_stb_i,          
-    input wire wbs_cyc_i,           
-    input [3:0] wbs_sel_i,           
-    output reg wbs_ack_o,  
-    input wire [31:0] wbs_dat_i,   
-    input wire [31:0] wbs_adr_i,   
-    output reg [31:0] wbs_dat_o,    
-    input wire wbs_we_i,             
+    input wire wbs_stb_i,
+    input wire wbs_cyc_i,
+    input [3:0] wbs_sel_i,
+    output reg wbs_ack_o,
+    input wire [31:0] wbs_dat_i,
+    input wire [31:0] wbs_adr_i,
+    output reg [31:0] wbs_dat_o,
+    input wire wbs_we_i,
 
-  
-    input wire [`BUS_MIPORT] mport_i, 
+
+    input wire [`BUS_MIPORT] mport_i,
     output reg [`BUS_MOPORT] mport_o
 );
 
@@ -35,18 +35,18 @@ module wb_to_pk_m
 
     always @ (*) begin
         wbs_stbN = 0;
-        wbs_dat_o = wbs_datN[word_offset]; 
+        wbs_dat_o = wbs_datN[word_offset];
 
-        if(word_offset < NUM_REGS) begin 
+        if(word_offset < NUM_REGS) begin
             if (word_offset == RDATA_INDEX) begin
                 wbs_stbN[RDATA_INDEX] = wbs_stb_i && rdata_ready;
             end
             else
                 wbs_stbN = {{(NUM_REGS-1){1'b0}}, wbs_stb_i} << word_offset; // shifts by offset, provides proper stb 00001, 00010, 00100, 01000, 10000
-            wbs_ack_o = wbs_ackN[word_offset];  
-        end 
+            wbs_ack_o = wbs_ackN[word_offset];
+        end
         else
-            wbs_ack_o = 0;  
+            wbs_ack_o = 0;
     end
 
 
@@ -64,15 +64,15 @@ module wb_to_pk_m
         .wbs_ack_o(wbs_ackN[STATUS_INDEX]),
         .wbs_dat_o(wbs_datN[STATUS_INDEX]),
 
-        .access_read_mask_i(32'h0000000F), 
+        .access_read_mask_i(32'h0000000F),
         .access_write_mask_i(32'h0000000F),
         .periph_read_mask_i(32'h0000000F),
 
-        .enable_prot_i(32'h00000000), 
-        .enable_i(0), 
+        .enable_prot_i(32'h00000000),
+        .enable_i(0),
 
-        .reg_i(status_reg), 
-        .reg_o() 
+        .reg_i(status_reg),
+        .reg_o()
     );
 
 
@@ -90,15 +90,15 @@ module wb_to_pk_m
         .wbs_ack_o(wbs_ackN[ADDR_INDEX]),
         .wbs_dat_o(wbs_datN[ADDR_INDEX]),
 
-        .access_read_mask_i(32'hFFFFFFFF), 
+        .access_read_mask_i(32'hFFFFFFFF),
         .access_write_mask_i(32'hFFFFFFFF),
         .periph_read_mask_i(0),
 
-        .enable_prot_i(32'h00000000), 
-        .enable_i(0), 
+        .enable_prot_i(32'h00000000),
+        .enable_i(0),
 
-        .reg_i(0), 
-        .reg_o(addr_reg) 
+        .reg_i(0),
+        .reg_o(addr_reg)
     );
 
 
@@ -116,15 +116,15 @@ module wb_to_pk_m
         .wbs_ack_o(wbs_ackN[WDATA_INDEX]),
         .wbs_dat_o(wbs_datN[WDATA_INDEX]),
 
-        .access_read_mask_i(32'hFFFFFFFF), 
+        .access_read_mask_i(32'hFFFFFFFF),
         .access_write_mask_i(32'hFFFFFFFF),
         .periph_read_mask_i(0),
 
-        .enable_prot_i(32'h00000000), 
-        .enable_i(0), 
+        .enable_prot_i(32'h00000000),
+        .enable_i(0),
 
-        .reg_i(0), 
-        .reg_o(wdata_reg) 
+        .reg_i(0),
+        .reg_o(wdata_reg)
     );
 
 
@@ -143,15 +143,15 @@ module wb_to_pk_m
         .wbs_ack_o(wbs_ackN[WCOUNT_INDEX]),
         .wbs_dat_o(wbs_datN[WCOUNT_INDEX]),
 
-        .access_read_mask_i(32'hFFFFFFFF), 
+        .access_read_mask_i(32'hFFFFFFFF),
         .access_write_mask_i(32'hFFFFFFFF),
         .periph_read_mask_i(32'h00000000),
 
-        .enable_prot_i(32'h00000000), 
-        .enable_i(0), 
+        .enable_prot_i(32'h00000000),
+        .enable_i(0),
 
-        .reg_i(wcount_inc_reg), 
-        .reg_o(wcount_reg) 
+        .reg_i(wcount_inc_reg),
+        .reg_o(wcount_reg)
     );
 
 
@@ -169,25 +169,25 @@ module wb_to_pk_m
         .wbs_ack_o(wbs_ackN[RDATA_INDEX]),
         .wbs_dat_o(wbs_datN[RDATA_INDEX]),
 
-        .access_read_mask_i(32'hFFFFFFFF), 
+        .access_read_mask_i(32'hFFFFFFFF),
         .access_write_mask_i(32'hFFFFFFFF),
         .periph_read_mask_i(32'hFFFFFFFF),
 
-        .enable_prot_i(32'h00000000), 
-        .enable_i(0), 
+        .enable_prot_i(32'h00000000),
+        .enable_i(0),
 
         .reg_i(rdata_reg),
         .reg_o()
     );
 
 
-    localparam STANDBY = 0; 
+    localparam STANDBY = 0;
     localparam PK_WRITE_PREP  = 1;
     localparam PK_STREAM_WRITE = 2;
     localparam PK_READ_PREP = 3;
     localparam PK_WORD_READ = 4;
     localparam PK_CLEANUP = 5;
-    
+
     reg [5:0] state;
 
     always@ (posedge wb_clk_i or posedge wb_rst_i) begin
@@ -202,12 +202,12 @@ module wb_to_pk_m
         mport_o[`BUS_MO_ADDR] <= addr_reg;
 
 
-        case (state) 
+        case (state)
             STANDBY: begin  //wait until wishbone master wants to use bridge
                 status_reg <= STANDBY;
                 mport_o[`BUS_MO_REQ]  <= 0;
 
-                if ((wbs_ackN[WDATA_INDEX]) && wbs_we_i)    
+                if ((wbs_ackN[WDATA_INDEX]) && wbs_we_i)
                     state  <= PK_WRITE_PREP;
                 else if (wbs_stb_i && !wbs_we_i && !wbs_ack_o)
                     if (word_offset == RDATA_INDEX)
@@ -221,29 +221,35 @@ module wb_to_pk_m
                 mport_o[`BUS_MO_SIZE]   <= `BUS_SIZE_STREAM;
 
                 if (wcount_reg == 1) begin
-                    mport_o[`BUS_MO_SEQMST] <= 1; 
-                    
+                    mport_o[`BUS_MO_SEQMST] <= 1;
+
                     if (mport_i[`BUS_MI_ACK])
-                        state <= PK_CLEANUP;      
+                        state <= PK_CLEANUP;
                     else
                         state <= PK_WRITE_PREP;
                 end
-                else if(mport_i[`BUS_MI_ACK])
+                else begin
+                if(mport_i[`BUS_MI_ACK]) begin
                     state <= PK_STREAM_WRITE;
+                    // if(wcount_reg == 2)
+                    //     mport_o[`BUS_MO_SEQMST] <= 1;
+                end
                 else
                     state <= PK_WRITE_PREP;
+                end
             end
             PK_STREAM_WRITE: begin
                 status_reg <= PK_STREAM_WRITE;
-                
+
                 if (mport_i[`BUS_MI_SEQSLV]) begin
-                    if(wcount_inc_reg + 2 >= wcount_reg) begin
-                        mport_o[`BUS_MO_SEQMST] <= 1;
-                        state <= PK_CLEANUP;
-                    end
-                    else begin
+                    if(wcount_inc_reg < wcount_reg)
                         wcount_inc_reg <= wcount_inc_reg + 1;
                     end
+                else if (!mport_i[`BUS_MI_SEQSLV]) begin
+                    if(wcount_inc_reg >= wcount_reg)
+                        state <= PK_CLEANUP;
+                    else if(wcount_inc_reg + 1 >= wcount_reg)
+                        mport_o[`BUS_MO_SEQMST] <= 1;
                 end
             end
             PK_CLEANUP: begin
@@ -256,7 +262,7 @@ module wb_to_pk_m
                     state <= STANDBY;
                 end
             end
-            PK_READ_PREP: begin 
+            PK_READ_PREP: begin
                     status_reg <= PK_READ_PREP;
                     mport_o[`BUS_MO_REQ] <= 1;
                     mport_o[`BUS_MO_RW] <= `BUS_READ;
@@ -278,4 +284,3 @@ module wb_to_pk_m
     end
 end
 endmodule
-
